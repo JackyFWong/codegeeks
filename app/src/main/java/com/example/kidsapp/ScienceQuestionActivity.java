dd49f52e34c4f931d0c68a61272f52e32f03c945
ManishEnishetty <37671355+ManishEnishetty@users.noreply.github.com>
2021-04-15 14:19:49 -0400

    1st commit

---
+++ app/src/main/java/com/example/kidsapp/ScienceQuestionActivity.java
@@ -0,0 +1,14 @@
+package com.example.kidsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScienceQuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_science_question);
+    }
+}
